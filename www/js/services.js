de43eac872cca4b4dce5f5519126af01d945a6ae
Fengda HUANG <h@phodal.com>
2015-03-19 23:11:34 +0800

    Add blog lists

--- www/js/services.js
+++ www/js/services.js
@@ -3,29 +3,61 @@ angular.module('starter.services', [])
 .factory('Blog', function($http, $q) {
   var blogs = [{
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    image: 'img/homepage.png'
+    name: '前言'
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-	  image: 'img/homepage.png'
+    name: '无处不在的HTML'
   }, {
     id: 2,
-    name: 'Andrew Jostlin',
-    lastText: 'Did you get the ice cream?',
-	  image: 'img/homepage.png'
+    name: '无处不在的Javascript'
   }, {
     id: 3,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-	  image: 'img/homepage.png'
+    name: '无处不在的CSS'
   }, {
     id: 4,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-	  image: 'img/homepage.png'
+    name: '无处不在的三剑客'
+  },{
+    id: 5,
+    name: 'GNU/Linux 强大且Free'
+  }, {
+    id: 6,
+    name: 'Arduino 极客的玩具'
+  }, {
+    id: 8,
+    name: 'Raspberry Pi'
+  }, {
+    id: 9,
+    name: 'Server 一切皆为服务'
+  }, {
+    id: 10,
+    name: 'Web服务'
+  }, {
+    id: 11,
+    name: 'HTTP 熟悉&陌生'
+  }, {
+    id: 12,
+    name: '设计RESTful API'
+  }, {
+    id: 13,
+    name: '环境准备'
+  }, {
+    id: 14,
+    name: '创建REST服务'
+  },{
+    id: 15,
+    name: '前端显示'
+  }, {
+    id: 16,
+    name: 'RESTful的CoAP协议'
+  }, {
+    id: 17,
+    name: '使用IoT-CoAP构建物联网'
+  }, {
+    id: 18,
+    name: '简单物联网'
+  }, {
+    id: 19,
+    name: 'Android简单示例'
   }];
 
   return {
